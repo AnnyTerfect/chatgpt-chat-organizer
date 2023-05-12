efa3837235ed05844aecde009c09c34a23830a42
AnnyTerfect <1247470246@qq.com>
2023-05-12 11:54:49 +0800

    refactor: modulize debounce

---
+++ src/utils/debounce.js
@@ -0,0 +1,16 @@
+function debounce(func, delay) {
+  let timerId;
+
+  return function () {
+    const context = this;
+    const args = arguments;
+
+    clearTimeout(timerId);
+
+    timerId = setTimeout(function () {
+      func.apply(context, args);
+    }, delay);
+  };
+}
+
+export default debounce;
